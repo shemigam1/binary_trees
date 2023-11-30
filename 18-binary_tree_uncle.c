e0d0a75f88b97d54eb29d8a637af2bbb4808cabb
semilore <semilore@omotade-michaels.com>
2023-11-30 20:02:16 +0100

    feat: check uncle of node

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - finds uncle of node
+ * @node: node
+ * Return: uncle
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+
+	if (node->parent->parent == NULL)
+		return (NULL);
+	if (node->parent->parent->left == node->parent)
+	{
+		if (node->parent->parent->right != NULL)
+			return (node->parent->parent->right);
+		else
+			return (NULL);
+	}
+	if (node->parent->parent->right == node->parent)
+		return (node->parent->parent->left);
+	else
+		return (NULL);
+}
