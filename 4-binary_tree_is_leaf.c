dc12d37280ab2fa27a06a1e2c5a58f01c06fe460
semilore <semilore@omotade-michaels.com>
2023-11-30 11:42:35 +0100

    feat: checks if a node is a leaf

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - checks if node is a leaf
+ * @node: node
+ * Return: 1 on success 0 otherwise
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	else
+		return (0);
+}
