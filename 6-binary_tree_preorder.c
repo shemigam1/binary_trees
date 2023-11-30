275373857d79d454b1f657085463ca55e45e274e
semilore <semilore@omotade-michaels.com>
2023-11-30 15:03:34 +0100

    feat: preorder traversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverses binary tree with pre-order method
+ * @tree: tree
+ * Return: void
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree != NULL || func != NULL)
+	{
+		func(tree->n);
+		if (tree->left != NULL)
+			binary_tree_preorder(tree->left, func);
+		if (tree->right != NULL)
+			binary_tree_preorder(tree->right, func);
+	}
+}
