a46420db8b917b4e89eec9c6ad86136b386b73ae
semilore <semilore@omotade-michaels.com>
2023-11-30 11:03:45 +0100

    feat: add new node to left of parent

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = NULL;
 	binary_tree_t *temp = parent->left;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL || parent == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	new_node->parent = parent;
 	new_node->n = value;
