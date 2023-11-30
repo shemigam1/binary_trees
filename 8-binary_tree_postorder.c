90c87fcaadcc3f71fadc5d3981ef877b158e9e90
semilore <semilore@omotade-michaels.com>
2023-11-30 15:19:32 +0100

    fix: logical operator

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_postorder - postorder traversal
+ * @tree: ptr to root
+ * @func: function
+ */
+
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree
+}
