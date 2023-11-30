0bf17749d5791cbed9f8b52080317201a558b5ae
semilore <semilore@omotade-michaels.com>
2023-11-30 11:12:57 +0100

    fix: proper pointer allocation

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,24 +10,30 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node = NULL;
-	binary_tree_t *temp = parent->left;
 
 	if (parent == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node->parent = parent;
+
 	new_node->n = value;
+	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+
 	if (parent->left == NULL)
 	{
 		parent->left = new_node;
 	}
+	else
+	{
+		binary_tree_t *temp = parent->left;
 
-	new_node->left = temp;
-	parent->left->parent = new_node;
-	parent->left = new_node;
+		temp->parent = new_node;
+		new_node->left = temp;
+		parent->left = new_node;
+	}
 	return (new_node);
 }
